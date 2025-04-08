13efb8dec95bc41b11e53f4bf7672825a9de0606
viktor-webdev983 <webdev983@gmail.com>
2025-04-08 22:32:51 +0300

    add attrs

--- Tag.php
+++ Tag.php
@@ -3,19 +3,35 @@
 class Tag {
 	
 	private $name;
+	private $attrs;
 
-	public function __construct($name) {
+	public function __construct($name, $attrs = '') {
 		$this->name = $name;
+		$this->attrs = $attrs;
 	}
 
 	public function open() {
 		$name = $this->name;
-		return "<$name>";
+		$attrsStr = getAttrsStr($this->attrs);
+		return "<$name$attrsStr>";
 	}
 	
 	public function close() {
 		$name = $this->name;
 		return "</$name>";
+	}
+
+	private function getAttrsStr($attrs) {
+		if(!empty($attrs)) {
+			$result = '';
+
+			foreach($attrs as $name => $value) {
+				$result .= " $name=\"$value\"";
+			}
+			return $result;
+		} else {
+			return '';
+		}
 	}
 }
 
