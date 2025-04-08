acaee33cf04508f374fdcaa6fea236dd7378b2bd
viktor-webdev983 <webdev983@gmail.com>
2025-04-08 14:54:19 +0300

    added Date class

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
-<?php echo 'test'; ?>
-<?php $arr = []; ?>
+<?php
+class Date {
+
+    private $date;
+
+    public function __construct($date = null) {
+        if($date === null) {
+            $this->date = new DateTime();
+        } else {
+            $this->date = new DateTime($date);
+        }
+    }
+
+    public function getDay() {
+        return $this->date->format('d');
+    }
+}
+
+$date = new Date('2025-04-08');
+
+echo $date->getDay();
+
+
+?>
