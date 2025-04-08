50413e4673e94b37f48ff161cf78763ece82a2f5
viktor-webdev983 <webdev983@gmail.com>
2025-04-08 15:04:30 +0300

    added month

--- index.php
+++ index.php
@@ -14,11 +14,52 @@ class Date {
     public function getDay() {
         return $this->date->format('d');
     }
+
+    public function getMonth($lang = null) {
+        $month = $this->date->format('m');
+
+        if($lang === 'ua') {
+            $months = [
+                '01' => 'Січень', 
+                '02' => 'Лютий', 
+                '03' => 'Березень', 
+                '04' => 'Квітень', 
+                '05' => 'Травень', 
+                '06' => 'Червень', 
+                '07' => 'Липень', 
+                '08' => 'Серпень', 
+                '09' => 'Вересень', 
+                '10' => 'Жовтень', 
+                '11' => 'Листопад', 
+                '12' => 'Грудень', 
+            ];
+            return $months[$month];
+        } elseif($lang === 'en') {
+            $months = [
+                '01' => 'January', 
+                '02' => 'February', 
+                '03' => 'March', 
+                '04' => 'April', 
+                '05' => 'May', 
+                '06' => 'June', 
+                '07' => 'Jule', 
+                '08' => 'August', 
+                '09' => 'September', 
+                '10' => 'October', 
+                '11' => 'November', 
+                '12' => 'December', 
+            ];
+            return $months[$month];
+        }
+        return $month;
+    }
+
 }
 
 $date = new Date('2025-04-08');
 
 echo $date->getDay();
+echo $date->getMonth('ua');
 
 
 ?>
