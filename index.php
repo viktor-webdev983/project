d127631245b4ecd70b073e9e661ff51a881480cc
viktor-webdev983 <webdev983@gmail.com>
2025-04-09 01:16:15 +0300

    edit

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ echo (new Date('2025-12-31'))->subDay(3)->addYear(1); // '2026-12-28'*/
 $tag = new Tag('div');
 echo $tag->setAttr('id', 'mu_id')->setAttr('class', 'mu_class')->open() . 'text' . $tag->close();
 
-$tag->addClass('new_class mu_class');
+$tag->setAttr('class', 'mu_class')->addClass('new_class mu_class');
 
 echo $tag->open() . 'test2' . $tag->close();
 ?>
