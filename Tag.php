9933444f172703eedb840f4bb198a5362f332e6e
viktor-webdev983 <webdev983@gmail.com>
2025-04-09 01:36:46 +0300

    edit

--- Tag.php
+++ Tag.php
@@ -1,6 +1,47 @@
 <?php
+interface iTag
+{
+	// Геттер имени:
+	public function getName();
+	
+	// Геттер текста:
+	public function getText();
+	
+	// Геттер всех атрибутов:
+	public function getAttrs();
+	
+	// Геттер одного атрибута по имени:
+	public function getAttr($name);
+	
+	// Открывающий тег, текст и закрывающий тег:
+	public function show();
+	
+	// Открывающий тег:
+	public function open();
+	
+	// Закрывающий тег:
+	public function close();
+	
+	// Установка текста:
+	public function setText($text);
+	
+	// Установка атрибута:
+	public function setAttr($name, $value = true);
+	
+	// Установка атрибутов:
+	public function setAttrs($attrs);
+	
+	// Удаление атрибута:
+	public function removeAttr($name);
+	
+	// Установка класса:
+	public function addClass($className);
+	
+	// Удаление класса:
+	public function removeClass($className);
+}
 
-class Tag {
+class Tag implements iTag {
 	
 	private $name;
 	private $text;
@@ -45,6 +86,18 @@ class Tag {
 			$this->setAttr($name, $value);
 		}
 		return $this;
+	}
+
+	public function setText($text) {
+		return $this->setText($text);
+	}
+
+	public function show() {
+		if(isset($this->text)) {
+			return $this->open().$this->text.$this->close();
+		} else {
+			return '';
+		}
 	}
 
 	public function removeAttr($name) {
