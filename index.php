6d04b27531f77642e10e29885ad8eef00f16705c
viktor-webdev983 <webdev983@gmail.com>
2025-04-13 18:00:53 +0300

    edit

--- index.php
+++ index.php
@@ -32,8 +32,7 @@ $list = new HtmlList('ul');
 echo $list
 ->addItem((new ListItem())->setText('item1'))
 ->addItem((new ListItem())->setText('item2'))
-->addItem((new ListItem())->setText('item3'))
-->show();
+->addItem((new ListItem())->setText('item3'));
 
 
 ?>
