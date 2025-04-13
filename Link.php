ca4f4ca4e285e85079da92bc6e43c19f919adec1
viktor-webdev983 <webdev983@gmail.com>
2025-04-13 16:38:47 +0300

    edit

--- Link.php
+++ Link.php
@@ -6,4 +6,15 @@ class Link extends Tag {
 
         parent::__construct('a');
     }
+
+    public function open() {
+        $this->activateSelf();
+        return parent::open();
+    }
+
+    private function activateSelf() {
+        if($this->getAttr('href') === $_SERVER['REQUEST_URI']) {
+            $this->addClass('active');
+        }
+    }
 }
