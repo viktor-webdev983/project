ac7a99e9040daf5a45ecb80f68cbaf5a94d30b59
viktor-webdev983 <webdev983@gmail.com>
2025-04-13 16:21:49 +0300

    edit

--- index.php
+++ index.php
@@ -20,5 +20,5 @@ echo (new Date('2025-12-31'))->addDay(1);  // '2026-01-01'
 echo (new Date('2025-12-31'))->subDay(3)->addYear(1); // '2026-12-28'*/
 
 
-echo (new Image())->setAttrs(['width'=>'200','height'=>'300'])->open();
+echo (new Image())->setAttrs(['width'=>'200','height'=>'300']);
 ?>
