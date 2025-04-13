29b8752015679d92dcb31521363c00867010cd19
viktor-webdev983 <webdev983@gmail.com>
2025-04-13 16:44:34 +0300

    edit

--- menu.php
+++ menu.php
@@ -5,7 +5,7 @@ require_once('Link.php');
 
 
 echo (new Link())->setText('index')->setAttr('href', '1.php')->show();
-echo (new Link())->setText('index')->setAttr('href', '2.php')->show();
-echo (new Link())->setText('index')->setAttr('href', '3.php')->show();
-echo (new Link())->setText('index')->setAttr('href', '4.php')->show();
-echo (new Link())->setText('index')->setAttr('href', '5.php')->show();
+echo (new Link())->setText('page')->setAttr('href', '2.php')->show();
+echo (new Link())->setText('blog')->setAttr('href', '3.php')->show();
+echo (new Link())->setText('contact')->setAttr('href', '4.php')->show();
+echo (new Link())->setText('forum')->setAttr('href', '5.php')->show();
