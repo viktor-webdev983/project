9fd61aa3e238da8ed1fdba9fb5745b8ebbe298c0
viktor-webdev983 <webdev983@gmail.com>
2025-04-13 16:48:34 +0300

    fix

--- menu.php
+++ menu.php
@@ -4,8 +4,8 @@ require_once 'Tag.php';
 require_once('Link.php');
 
 
-echo (new Link())->setText('index')->setAttr('href', '1.php')->show();
-echo (new Link())->setText('page')->setAttr('href', '2.php')->show();
-echo (new Link())->setText('blog')->setAttr('href', '3.php')->show();
-echo (new Link())->setText('contact')->setAttr('href', '4.php')->show();
-echo (new Link())->setText('forum')->setAttr('href', '5.php')->show();
+echo (new Link())->setText('index')->setAttr('href', '/1.php')->show();
+echo (new Link())->setText('page')->setAttr('href', '/2.php')->show();
+echo (new Link())->setText('blog')->setAttr('href', '/3.php')->show();
+echo (new Link())->setText('contact')->setAttr('href', '/4.php')->show();
+echo (new Link())->setText('forum')->setAttr('href', '/5.php')->show();
